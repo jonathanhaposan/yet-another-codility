adc9ff549175ce2a1249870bd8b4dd4ae90ca8bd
Jonathan Situmorang <jonathan.situmorang@go-jek.com>
2021-10-28 22:45:16 +0700

    add comment

--- zirconium2019/solution.go
+++ zirconium2019/solution.go
@@ -108,10 +108,29 @@ func Solution2(A []int, B []int, F int) int {
 
 	sort.Ints(differenceList)
 
-	sumWeakContributionFE := 0
+	sumLowestDiff := 0
 	for i := 0; i < F; i++ {
-		sumWeakContributionFE += differenceList[i]
+		// why loop using F, because the differenceList sorted
+		// from lowest to highest difference score in another word
+		// lowest diff score mean highest FE score
+		// highest diff score mean highest BE score
+		sumLowestDiff += differenceList[i]
 	}
 
-	return sumAllBE - sumWeakContributionFE
+	// A 4, 2, 1
+	// B 2, 5, 3
+	//
+	// B - A
+	// -2, 3, 2
+	//
+	// sort
+	// -2, 2, 3
+	//
+	// sumAllBE - sumLowestDiff
+	// (2 + 3 + 5) - (-2 + 2)
+	// or in can be like
+	// (2 - (-2)) + (3 - (2)) + 5
+	// A[0] + A[2] + B[1]
+
+	return sumAllBE - sumLowestDiff
 }
